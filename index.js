e5ae4cc5959f182d430ee8c6001eaf1e849b6533
Moksh parashar <parasharm239@gmail.com>
2024-02-21 07:25:53 -0800

    .

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const data = require("./json");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 console.log(data);
+const corsOptions = {
+  origin: "https://worldFactz.netlify.app",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -16,6 +22,7 @@ app.get("/login", (req, res) => {
 app.get("/api/adarsh", (req, res) => {
   res.json(data);
 });
+app.use(cors(corsOptions));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
